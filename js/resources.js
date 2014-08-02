c14803a31c3095ac9f6ef685b0d21e1b659bdbfe
Phodal Huang <h@phodal.com>
2014-08-02 22:44:43 +0800

    First init

---
+++ js/resources.js
@@ -0,0 +1,29 @@
+game.resources = [
+
+    {name: "ScreenGirl", type:"image", src: "data/img/screengirl.png"}
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
