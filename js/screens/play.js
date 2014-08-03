506d07b2ae61584a5e89efb95d59f5127811ac4e
Phodal Huang <h@phodal.com>
2014-08-03 10:57:54 +0800

    Add the text of this

--- js/screens/play.js
+++ js/screens/play.js
@@ -1,18 +1,39 @@
 game.PlayScreen = me.ScreenObject.extend({
 
-	onResetEvent: function() {
-		game.data.score = 0;
+    onResetEvent: function() {
+        game.data.score = 0;
 
         me.game.world.addChild(new me.ColorLayer("background", "#c69329", 0));
         var screenGirl = new me.ImageLayer("ScreenGirl", 262, 607, "ScreenGirl", 3, 1);
         me.game.world.addChild(screenGirl);
         me.game.world.addChild(new me.ImageLayer("bgColor2", 960, 480, "bgColor2", 2, 1));
 
-        this.HUD = new game.HUD.Container();
-		me.game.world.addChild(this.HUD);
-	},
+        me.game.world.addChild(new TextLogo(me.video.getWidth(), me.video.getHeight()), 4);
 
-	onDestroyEvent: function() {
-		me.game.world.removeChild(this.HUD);
-	}
+        this.HUD = new game.HUD.Container();
+        me.game.world.addChild(this.HUD);
+    },
+
+    onDestroyEvent: function() {
+        me.game.world.removeChild(this.HUD);
+    }
+});
+
+
+var TextLogo = me.Renderable.extend({
+    init: function(w, h) {
+        this.parent(new me.Vector2d(), w, h);
+        this.logo1 = new me.Font("Arial", 64, "white", "middle");
+        this.logo1.textBaseline = "alphabetic";
+    },
+
+    draw: function(context) {
+        var geekText = "极客爱情";
+        var logo1_width = this.logo1.measureText(context, geekText).width;
+        var xpos = (this.width - logo1_width) / 2;
+        var ypos = (this.height / 2);
+
+        this.logo1.draw(context, geekText, xpos, ypos);
+    }
+
 });
