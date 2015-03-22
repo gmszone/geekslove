27d0c855ca7c2ebfed7125f33fa650935bac22c0
Fengda HUANG <h@phodal.com>
2015-03-22 13:50:00 +0800

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,114 @@
+angular.module('starter', ['ionic','hc.marked', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (typeof analytics !== 'undefined'){
+      analytics.startTrackerWithId('UA-60917659-4');
+    } else {
+      console.log("Google Analytics plugin could not be loaded.")
+    }
+    if(window.plugins && window.plugins.AdMob) {
+      var admob_key = device.platform == "Android" ? "ANDROID_PUBLISHER_KEY" : "IOS_PUBLISHER_KEY";
+      var admob = window.plugins.AdMob;
+      admob.createBannerView(
+        {
+          'publisherId': 'ca-app-pub-3662578183051823/3416509805',
+          'adSize': admob.AD_SIZE.BANNER,
+          'bannerAtTop': false
+        },
+        function() {
+          admob.requestAd(
+            { 'isTesting': false },
+            function() {
+              admob.showAd(true);
+            },
+            function() { console.log('failed to request ad'); }
+          );
+        },
+        function() { console.log('failed to create banner view'); }
+      );
+    }
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(['markedProvider', function (markedProvider) {
+  markedProvider.setOptions({
+    gfm: true,
+    tables: true,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    }
+  })
+}])
+
+.config(function($ionicConfigProvider){
+	$ionicConfigProvider.tabs.position('bottom');
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+
+  .state('tab.home', {
+    url: '/home',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/tab-homepage.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('tab.blog', {
+      url: '/blog',
+      views: {
+        'tab-blog': {
+          templateUrl: 'templates/tab-blog.html',
+          controller: 'BlogCtrl'
+        }
+      }
+    })
+    .state('tab.blog-detail', {
+      url: '/blog/:blogId',
+      views: {
+        'tab-blog': {
+          templateUrl: 'templates/blog-detail.html',
+          controller: 'BlogDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-about.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  })
+
+  .state('tab.flask', {
+    url: '/flask',
+    views: {
+      'tab-flask': {
+        templateUrl: 'templates/tab-flask.html',
+        controller: 'FlaskCtrl'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/tab/home');
+
+});
---
+++ www/js/services.js
@@ -0,0 +1,113 @@
+angular.module('starter.services', [])
+
+.factory("transformRequestAsFormPost", function() {
+    function transformRequest( data, getHeaders ) {
+      var headers = getHeaders();
+      headers[ "Content-type" ] = "application/x-www-form-urlencoded; charset=utf-8";
+      return( serializeData( data ) );
+    }
+    return( transformRequest );
+    function serializeData( data ) {
+      if ( ! angular.isObject( data ) ) {
+        return( ( data == null ) ? "" : data.toString() );
+      }
+      var buffer = [];
+      for ( var name in data ) {
+        if ( ! data.hasOwnProperty( name ) ) {
+          continue;
+        }
+        var value = data[ name ];
+        buffer.push(
+          encodeURIComponent( name ) + "=" + encodeURIComponent( ( value == null ) ? "" : value )
+        );
+      }
+      var source = buffer.join( "&" ).replace( /%20/g, "+" );
+      return( source );
+    }
+  }
+)
+
+.factory('Blog', function($http, $q) {
+  var blogs = [{
+    id: 0,
+    name: '前言'
+  }, {
+    id: 1,
+    name: '无处不在的HTML'
+  }, {
+    id: 2,
+    name: '无处不在的Javascript'
+  }, {
+    id: 3,
+    name: '无处不在的CSS'
+  }, {
+    id: 4,
+    name: '无处不在的三剑客'
+  },{
+    id: 5,
+    name: 'GNU/Linux 强大且Free'
+  }, {
+    id: 6,
+    name: 'Arduino 极客的玩具'
+  }, {
+    id: 8,
+    name: 'Raspberry Pi'
+  }, {
+    id: 9,
+    name: 'Server 一切皆为服务'
+  }, {
+    id: 10,
+    name: 'Web服务'
+  }, {
+    id: 11,
+    name: 'HTTP 熟悉&陌生'
+  }, {
+    id: 12,
+    name: '设计RESTful API'
+  }, {
+    id: 13,
+    name: '环境准备'
+  }, {
+    id: 14,
+    name: '创建REST服务'
+  },{
+    id: 15,
+    name: '前端显示'
+  }, {
+    id: 16,
+    name: 'RESTful的CoAP协议'
+  }, {
+    id: 17,
+    name: '使用IoT-CoAP构建物联网'
+  }, {
+    id: 18,
+    name: '简单物联网'
+  }, {
+    id: 19,
+    name: 'Android简单示例'
+  }];
+
+  return {
+    all: function() {
+      return blogs;
+    },
+	  async: function(file_name) {
+		  var def = $q.defer();
+		  $http.get('assets/' + file_name + '.md')
+			  .success(function (response) {
+				  def.resolve(response);
+			  }).error(function () {
+				  def.reject("Failed to get albums");
+			  });
+		  return def.promise;
+	  },
+    get: function(blogId) {
+      for (var i = 0; i < blogs.length; i++) {
+        if (blogs[i].id === parseInt(blogId)) {
+          return blogs[i];
+        }
+      }
+      return null;
+    }
+  };
+});
