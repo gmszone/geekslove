7b8c5579557ae4f01154fdb7918c1f6ab2a65411
Fengda HUANG <h@phodal.com>
2015-03-22 14:06:51 +0800

    remove some unuse function

--- www/js/app.js
+++ www/js/app.js
@@ -2,32 +2,6 @@ angular.module('starter', ['ionic','hc.marked', 'starter.controllers', 'starter.
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if (typeof analytics !== 'undefined'){
-      analytics.startTrackerWithId('UA-60917659-4');
-    } else {
-      console.log("Google Analytics plugin could not be loaded.")
-    }
-    if(window.plugins && window.plugins.AdMob) {
-      var admob_key = device.platform == "Android" ? "ANDROID_PUBLISHER_KEY" : "IOS_PUBLISHER_KEY";
-      var admob = window.plugins.AdMob;
-      admob.createBannerView(
-        {
-          'publisherId': 'ca-app-pub-3662578183051823/3416509805',
-          'adSize': admob.AD_SIZE.BANNER,
-          'bannerAtTop': false
-        },
-        function() {
-          admob.requestAd(
-            { 'isTesting': false },
-            function() {
-              admob.showAd(true);
-            },
-            function() { console.log('failed to request ad'); }
-          );
-        },
-        function() { console.log('failed to create banner view'); }
-      );
-    }
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -98,16 +72,6 @@ angular.module('starter', ['ionic','hc.marked', 'starter.controllers', 'starter.
       }
     }
   })
-
-  .state('tab.flask', {
-    url: '/flask',
-    views: {
-      'tab-flask': {
-        templateUrl: 'templates/tab-flask.html',
-        controller: 'FlaskCtrl'
-      }
-    }
-  });
 
   $urlRouterProvider.otherwise('/tab/home');
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,33 +2,6 @@ angular.module('starter.controllers', [])
 
 .controller('HomeCtrl', function($scope) {})
 
-.controller('FlaskCtrl', function($scope, $http, transformRequestAsFormPost) {
-    $scope.master = {};
-    $scope.url = 'http://mqtt.phodal.com/topics/';
-
-    $scope.get = function(uid) {
-      $http.get($scope.url + uid).success(function(data){
-        $scope.master = angular.copy(data);
-        console.log($scope.master);
-      });
-    };
-    $scope.post = function(data) {
-      $scope.master = angular.copy(data);
-      var url = $scope.url + data.uid,
-        data = data.data,
-        postCfg = {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-          transformRequest: transformRequestAsFormPost
-        };
-
-      console.log(url,data);
-      $http.post(url, data, postCfg)
-        .success(function(){
-          $scope.master = {'status': 'success'};
-        });
-    };
-  })
-
 .controller('BlogCtrl', function($scope, Blog) {
   $scope.blogs = Blog.all();
 })
