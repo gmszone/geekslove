880bcd83dc8cb1521c139fbd22e5d0fe117b8214
Fengda HUANG <h@phodal.com>
2015-03-22 20:21:26 +0800

    Add app rate

--- www/js/app.js
+++ www/js/app.js
@@ -1,81 +1,91 @@
-angular.module('starter', ['ionic','ngCordova', 'hc.marked', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngCordova', 'hc.marked', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .run(function ($cordovaSplashscreen) {
+    setTimeout(function () {
+      $cordovaSplashscreen.hide()
+    }, 2000)
+  })
+
+  .config(['markedProvider', function (markedProvider) {
+    markedProvider.setOptions({
+      gfm: true,
+      tables: true
+    })
+  }])
+
+  .config(function ($cordovaAppRateProvider) {
+    document.addEventListener("deviceready", function () {
+      var prefs = {
+        language: 'zh-Hans',
+        appName: '极客爱情',
+        androidURL: 'market://details?id=com.phodal.geekslove'
+      };
+      $cordovaAppRateProvider.setPreferences(prefs)
+    }, false);
+  })
+
+  .config(function ($ionicConfigProvider) {
+    $ionicConfigProvider.tabs.position('bottom');
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+
+      .state('tab.home', {
+        url: '/home',
+        views: {
+          'tab-home': {
+            templateUrl: 'templates/tab-homepage.html',
+            controller: 'HomeCtrl'
+          }
+        }
+      })
+
+      .state('tab.blog', {
+        url: '/blog',
+        views: {
+          'tab-blog': {
+            templateUrl: 'templates/tab-blog.html',
+            controller: 'BlogCtrl'
+          }
+        }
+      })
+      .state('tab.blog-detail', {
+        url: '/blog/:blogId',
+        views: {
+          'tab-blog': {
+            templateUrl: 'templates/blog-detail.html',
+            controller: 'BlogDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-about.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      })
+    $urlRouterProvider.otherwise('/tab/home');
+
   });
-})
-
-.run(function($cordovaSplashscreen) {
-	setTimeout(function() {
-		$cordovaSplashscreen.hide()
-	}, 2000)
-})
-
-.config(['markedProvider', function (markedProvider) {
-  markedProvider.setOptions({
-    gfm: true,
-    tables: true
-  })
-}])
-
-.config(function($ionicConfigProvider){
-	$ionicConfigProvider.tabs.position('bottom');
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  .state('tab.home', {
-    url: '/home',
-    views: {
-      'tab-home': {
-        templateUrl: 'templates/tab-homepage.html',
-        controller: 'HomeCtrl'
-      }
-    }
-  })
-
-  .state('tab.blog', {
-      url: '/blog',
-      views: {
-        'tab-blog': {
-          templateUrl: 'templates/tab-blog.html',
-          controller: 'BlogCtrl'
-        }
-      }
-    })
-    .state('tab.blog-detail', {
-      url: '/blog/:blogId',
-      views: {
-        'tab-blog': {
-          templateUrl: 'templates/blog-detail.html',
-          controller: 'BlogDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-about.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  })
-
-  $urlRouterProvider.otherwise('/tab/home');
-
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,13 +6,23 @@ angular.module('starter.controllers', [])
   $scope.blogs = Blog.all();
 })
 
+.controller('LikeCtrl', function($scope, $cordovaAppRate) {
+    $scope.promptRating = function () {
+      $cordovaAppRate.promptForRating(true).then(function (result) {
+        console.log("result: " + result);
+        $scope.words = result;
+      }, function (error) {
+        $scope.words = result;
+      })
+    };
+})
+
+
 .controller('BlogDetailCtrl', function($scope, $stateParams, Blog) {
     $scope.md ='assets/' + $stateParams.blogId + '.md';
     $scope.blog = Blog.get($stateParams.blogId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function($scope, $log, $cordovaAppRate) {
+  $scope.words = '';
 });
